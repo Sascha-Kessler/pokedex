591964c79c57120b08631d2e3a409a0955739e70
SaschaKessler90 <sascha.k1809@gmail.com>
2025-08-22 16:53:26 +0200

    add PokemonCardOverlay

--- script.js
+++ script.js
@@ -62,3 +62,17 @@ function loadingSpinner() {
 function capitalizeFirstNameLetter(str = "") {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+function removeOverlayPokemonCards(index) {
+  document.getElementById('overlay').classList.remove('d_none');
+  let pokemonOverlay = document.getElementById('overlayCard');
+  pokemonOverlay.innerHTML = getPokemonCardOverlayTemplate(index); 
+}
+
+function closeOverlay() {
+  document.getElementById('overlay').classList.add('d_none');
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') closeOverlay();
+});
--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,17 @@
 function getPokemonCardTemplate(index){
-   return `<div class="pokemon_card pokemon_type_${mainType}">
+   return `<div class="pokemon_card pokemon_type_${mainType}" onclick="removeOverlayPokemonCards(${index})">
+            <div class="pokemon_card_header">
+                <div>#${pokemonLoad[index].id}</div>
+                <div>${capitalizeFirstNameLetter(pokemonLoad[index].name)}</div>
+            </div>
+            <img src="${pokemonLoad[index].sprites.front_default}" alt="">
+            <div>Pokemon Stats</div>
+        </div>`
+}
+
+function getPokemonCardOverlayTemplate(index) {
+    setPokemonCardColour(index);
+    return `<div class="pokemon_card pokemon_type_${mainType}">
             <div class="pokemon_card_header">
                 <div>#${pokemonLoad[index].id}</div>
                 <div>${capitalizeFirstNameLetter(pokemonLoad[index].name)}</div>
