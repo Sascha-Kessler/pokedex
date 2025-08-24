0175ff10df0fdb7e559535f72acb6ceed3572480
SaschaKessler90 <sascha.k1809@gmail.com>
2025-08-24 14:06:28 +0200

    formatter code

--- scripts/render.js
+++ scripts/render.js
@@ -7,17 +7,17 @@ function renderPokemonCards(startIndex = 0) {
   loadingSpinner();
 }
 
- async function getAllPokemonDetails(limit = 20, offset) {
+async function getAllPokemonDetails(limit = 20, offset) {
   let response = await fetch(`${BASE_URL}?limit=${limit}&offset=${offset}`);
   let responseToJson = await response.json();
   let pokemonResults = responseToJson.results;
   let details = await Promise.all(
-    pokemonResults.map(p => fetch (p.url).then(r =>{
-    if (!r.ok) throw new Error (`HTTP ${r.status} for ${p.url}`);
-    return  r.json();
-  }).catch(error => {console.warn('Detail failed:', p.url, error);
-      return null;})));
-  console.log("details", details);
-  details = details.filter(Boolean);
-  return details;
-} 
+    pokemonResults.map((p) =>
+      fetch(p.url)
+        .then((r) => {if (!r.ok) throw new Error(`HTTP ${r.status} for ${p.url}`);
+          return r.json();})
+        .catch((error) => {console.warn("Detail failed:", p.url, error);
+          return null;})
+    ));
+  return details.filter(Boolean);
+}
--- scripts/template.js
+++ scripts/template.js
@@ -1,26 +1,30 @@
-function getPokemonCardTemplate(index){
-    setPokemonCardColour(index)
-   return `<div class="pokemon_card pokemon_type_${mainType}" onclick="openOverlayPokemonCards(${index})">
+function getPokemonCardTemplate(index) {
+  setPokemonCardColour(index);
+  return `<div class="pokemon_card pokemon_type_${mainType}" onclick="openOverlayPokemonCards(${index})">
             <div class="pokemon_card_header">
                 <div>#${pokemonLoad[index].id}</div>
                 <div>${capitalizeFirstNameLetter(pokemonLoad[index].name)}</div>
             </div>
             <img src="${pokemonLoad[index].sprites.front_default}" alt="">
-            <div>${capitalizeFirstNameLetter(pokemonLoad[index].types[0].type.name)}</div>
-        </div>`
+            <div>${capitalizeFirstNameLetter(
+              pokemonLoad[index].types[0].type.name
+            )}</div>
+        </div>`;
 }
 
 function getPokemonCardOverlayTemplate(index) {
-    setPokemonCardColour(index);
-    return `<div class="pokemon_card pokemon_card_overlay pokemon_type_${mainType}">
+  setPokemonCardColour(index);
+  return `<div class="pokemon_card pokemon_card_overlay pokemon_type_${mainType}">
             <div class="pokemon_card_header">
                 <div>#${pokemonLoad[index].id}</div>
-                <div id="pokemon_name">${capitalizeFirstNameLetter(pokemonLoad[index].name)}</div>
+                <div id="pokemon_name">${capitalizeFirstNameLetter(
+                  pokemonLoad[index].name
+                )}</div>
             </div>
             <img src="${pokemonLoad[index].sprites.front_default}" alt="">
             <div class="overlay_navigation_btn_container">
                 <button id="previous_btn" class="poke_btn" onclick="navigateOverlayPokemon(${index}, -1)">Catch previous</button>
                 <button id="next_btn" class="poke_btn" onclick="navigateOverlayPokemon(${index}, 1)">Catch next</button>
             </div>
-        </div>`
+        </div>`;
 }
